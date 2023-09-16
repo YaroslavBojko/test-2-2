cdb43caff87de3ef2ce4a3f506014df231a2c91a
Yaroslav Boyko <yaroslav.bojko.82@bk.ru>
2023-09-16 13:50:45 +0300

    proba

---
+++ test 2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <map>
+
+std::map<std::string, int> databaseEntry(std::map<std::string, int>& database)
+{
+    std::cout << "Fill out the store database!!!" << std::endl;
+    std::string vendorСode;
+    int quantity;
+
+    for (int i = 0; i < 5; ++i)
+    {
+        std::cout << "Enter a pair of article and number of pieces: " << std::endl;
+        std::cin >> vendorСode >> quantity;
+
+        database.insert(std::pair<std::string, int> (vendorСode, quantity));
+    }
+
+    return database;
+}
+
+void print(std::map<std::string, int>& database)
+{
+    for(std::map<std::string, int>::iterator it = database.begin(); it != database.end(); ++it)
+    {
+        std::cout << it->first << " " << it->second << std::endl;
+    }
+}
+
+int main() {
+    std::map<std::string, int> database;
+
+    databaseEntry(database);
+    print(database);
+
+}
